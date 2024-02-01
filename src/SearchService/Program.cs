e199caac1dab5d7ad7293c0dad00c6bc9682611a
sasha checheliuk <sasha.checheliuk@gmil.com>
2024-02-02 01:03:07 +0200

    Update docker-compose.yml

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
 using SearchService.Data;
@@ -7,6 +8,13 @@ using SearchService.Services;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 var app = builder.Build();
 app.UseAuthorization();
 app.MapControllers();
