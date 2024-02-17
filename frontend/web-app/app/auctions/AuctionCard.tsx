a7ba0b4176f53bf91f649126074699e09d3cd233
sasha checheliuk <sasha.checheliuk@gmil.com>
2024-02-17 02:11:18 +0200

    Update FE

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,24 +1,18 @@
-import Image from 'next/image'
 import React from 'react'
 import CountDownTimer from './CountDownTimer';
+import CarImage from './CarImage';
+import { Auction } from '@/types';
 
 type Props = {
-    auctions: any
+    auctions: Auction
 }
 
 export default function AuctionCard({auctions}: Props) {
   return (
-    <a href="#">
+    <a href="#" className='group'>
         <div className='w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg owerflow-hidden'>
             <div>
-                <Image 
-                    src={auctions.imageUrl}
-                    alt='image'
-                    fill
-                    className='object-cover'
-                    sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw'
-                    priority
-                />
+                <CarImage imageUrl={auctions.imageUrl} />
                 <div className='absolute bottom-2 left-2'>
                     <CountDownTimer auctionEnd={auctions.auctionEnd} />
                 </div>
--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -2,7 +2,6 @@ import Listings from "./auctions/Listings";
 
 
 export default function Home() {
-  console.log("Server Component");
   return (
     <div>
       <Listings />
