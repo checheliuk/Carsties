b8ca975c18620f9387734fa30882ef7ef25cda45
sasha checheliuk <sasha.checheliuk@gmil.com>
2024-02-18 21:16:33 +0200

    Add empty fiiters

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -9,13 +9,16 @@ import Filters from './Filters';
 import { useParamsStore } from '../hooks/useParamsStore';
 import { shallow } from 'zustand/shallow';
 import qs from 'query-string';
+import EmptyFilter from '../components/EmptyFilter';
 
 export default function Listings() {
     const [data, setData] = useState<PagedResult<Auction>>();
     const params = useParamsStore(state => ({
         pageNumber: state.pageNumber,
         pageSize: state.pageSize,
-        searchTerm: state.searchTerm
+        searchTerm: state.searchTerm,
+        orderBy: state.orderBy,
+        filterBy: state.filterBy
     }), shallow)
     const setParams = useParamsStore(state => state.setParams);
     const url = qs.stringifyUrl({url:'', query: params});
@@ -35,16 +38,37 @@ export default function Listings() {
     return (
         <>
             <Filters  />
-            <div className='grid grid-cols-4 gap-6'>
-                {data.results.map(auction => (
-                  <AuctionCard auctions={auction} key={auction.id} />
-                ))}
-            </div>
-            <div className='flex justify-center mt-4'>
-                <AppPagination pagaChanged={setPageNumber} 
-                    currentPage={params.pageNumber} 
-                    pageCount={data.pageCount} />
-            </div>
+            {data.totalCount === 0 ? (
+                <EmptyFilter showReset />
+            ) : (
+                <>
+                    <div 
+                        className='
+                            grid 
+                            grid-cols-4 
+                            gap-6
+                        '
+                    >
+                        {data.results.map(auction => (
+                            <AuctionCard auctions={auction} key={auction.id} />
+                        ))}
+                    </div>
+                    <div 
+                        className='
+                            flex 
+                            justify-center 
+                            mt-4
+                        '
+                    >
+                        <AppPagination 
+                            pagaChanged={setPageNumber} 
+                            currentPage={params.pageNumber} 
+                            pageCount={data.pageCount} 
+                        />
+                    </div>
+                </>
+            )}
+            
         </>
     )
 }
--- frontend/web-app/app/nav/Search.tsx
+++ frontend/web-app/app/nav/Search.tsx
@@ -6,14 +6,15 @@ import { useParamsStore } from '../hooks/useParamsStore'
 
 export default function Search() {
     const setParams = useParamsStore(state => state.setParams);
-    const [value, setValue] = useState('');
+    const setSearchValue = useParamsStore(state => state.setSearchValue);
+    const searchValue = useParamsStore(state => state.searchValue);
     
     function onChange(event: any) {
-        setValue(event.target.value);
+        setSearchValue(event.target.value);
     }
 
     function search () {
-        setParams({searchTerm: value});
+        setParams({searchTerm: searchValue});
     }
 
     return (
@@ -32,6 +33,7 @@ export default function Search() {
                 onKeyDown={(e: any) => {
                     if (e.key === 'Enter') search();
                 }}
+                value={searchValue}
                 onChange={onChange}
                 type="text" 
                 placeholder='Search for cars by make, model or color'
